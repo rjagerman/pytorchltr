648ba7091b501e614449c4a02c45a78092ee1a76
Rolf Jagerman <rjagerman@gmail.com>
2019-12-07 19:13:47 +0100

    Add click simulation and corresponding tests.

---
+++ pytorchltr/click_simulation.py
@@ -0,0 +1,112 @@
+import torch as _torch
+from pytorchltr.util import mask_padded_values as _mask_padded_values
+
+
+def simulate_pbm(rankings, ys, n, relevance_probs, cutoff=None, eta=1.0):
+    """Simulates clicks according to a position-biased user model.
+
+    Arguments:
+        rankings: A tensor of size (batch_size, list_size) of rankings.
+        ys: A tensor of size (batch_size, list_size) of relevance labels.
+        n: A tensor of size (batch_size) indicating the nr docs per query.
+        relevance_prob: A tensor of size (max_relevance) where the entry at
+            index "i" indicates the probability of clicking a document with
+            relevance label "i" (given that it is observed).
+        cutoff: The maximum list size to simulate.
+        eta: The severity of position bias (0.0 = no bias)
+
+    Returns:
+        A tuple of two tensors of size (batch_size, list_size), where the first
+        indicates the clicks with 0.0 and 1.0 and the second indicates the
+        propensity of observing each document.
+    """
+    # Cutoff at n for observation probabilities.
+    if cutoff is not None:
+        n = _torch.min(_torch.ones_like(n) * cutoff, n)
+
+    # Compute position-biased observation probabilities.
+    ranks = 1.0 + _torch.arange(
+        rankings.shape[1], device=rankings.device, dtype=_torch.float)
+    obs_probs = 1.0 / (1.0 + ranks) ** eta
+    obs_probs = _torch.repeat_interleave(
+        obs_probs[None, :], rankings.shape[0], dim=0)
+    obs_probs = _mask_padded_values(obs_probs, n, mask_value=0.0, mutate=True)
+
+    # Compute relevance labels at every rank.
+    ranked_ys = _torch.gather(ys, 1, rankings)
+
+    # Compute click probabilities (given observed).
+    relevance_probs = _torch.repeat_interleave(
+        relevance_probs[None, :], rankings.shape[0], dim=0)
+    click_probs = _torch.gather(relevance_probs, 1, ranked_ys)
+
+    # Sample clicks from bernoulli distribution with probabilities.
+    clicks = _torch.bernoulli(click_probs * obs_probs)
+
+    # Invert back to regular ranking.
+    invert_ranking = _torch.argsort(rankings, dim=1)
+
+    # Return click realization and propensities.
+    return (
+        _torch.gather(clicks, 1, invert_ranking).to(dtype=_torch.long),
+        _torch.gather(obs_probs, 1, invert_ranking)
+    )
+
+
+def simulate_perfect(rankings, ys, n, cutoff=None):
+    """Simulates clicks according to a perfect user model.
+
+    Arguments:
+        rankings: A tensor of size (batch_size, list_size) of rankings.
+        ys: A tensor of size (batch_size, list_size) of relevance labels.
+        n: A tensor of size (batch_size) indicating the nr docs per query.
+        cutoff: The maximum list size to simulate.
+
+    Returns:
+        A tuple of two tensors of size (batch_size, list_size), where the first
+        indicates the clicks with 0.0 and 1.0 and the second indicates the
+        propensity of observing each document.
+    """
+    rel_probs = _torch.FloatTensor(
+        [0.0, 0.2, 0.4, 0.8, 1.0], device=rankings.device)
+    return simulate_pbm(rankings, ys, n, rel_probs, cutoff, 0.0)
+
+
+def simulate_position(rankings, ys, n, cutoff=None, eta=1.0):
+    """Simulates clicks according to a binary position-biased user model.
+
+    Arguments:
+        rankings: A tensor of size (batch_size, list_size) of rankings.
+        ys: A tensor of size (batch_size, list_size) of relevance labels.
+        n: A tensor of size (batch_size) indicating the nr docs per query.
+        cutoff: The maximum list size to simulate.
+        eta: The severity of position bias (0.0 = no bias)
+
+    Returns:
+        A tuple of two tensors of size (batch_size, list_size), where the first
+        indicates the clicks with 0.0 and 1.0 and the second indicates the
+        propensity of observing each document.
+    """
+    rel_probs = _torch.FloatTensor(
+        [0.1, 0.1, 0.1, 1.0, 1.0], device=rankings.device)
+    return simulate_pbm(rankings, ys, n, rel_probs, cutoff, eta)
+
+
+def simulate_nearrandom(rankings, ys, n, cutoff=None, eta=1.0):
+    """Simulates clicks according to a near-random user model.
+
+    Arguments:
+        rankings: A tensor of size (batch_size, list_size) of rankings.
+        ys: A tensor of size (batch_size, list_size) of relevance labels.
+        n: A tensor of size (batch_size) indicating the nr docs per query.
+        cutoff: The maximum list size to simulate.
+        eta: The severity of position bias (0.0 = no bias)
+
+    Returns:
+        A tuple of two tensors of size (batch_size, list_size), where the first
+        indicates the clicks with 0.0 and 1.0 and the second indicates the
+        propensity of observing each document.
+    """
+    rel_probs = _torch.FloatTensor(
+        [0.4, 0.45, 0.5, 0.55, 0.6], device=rankings.device)
+    return simulate_pbm(rankings, ys, n, rel_probs, cutoff, eta)
---
+++ test/test_click_simulation.py
@@ -0,0 +1,238 @@
+import torch
+from pytorchltr.click_simulation import simulate_perfect
+from pytorchltr.click_simulation import simulate_position
+from pytorchltr.click_simulation import simulate_nearrandom
+from nose.tools import assert_true
+
+
+def _generate_test_data():
+    """Generates a small batch of test data for simulations."""
+    rankings = torch.LongTensor([
+        [3, 4, 0, 2, 1],
+        [1, 0, 2, 4, 3]
+    ])
+    ys = torch.LongTensor([
+        [1, 0, 4, 0, 2],
+        [4, 3, 0, 0, 0]
+    ])
+    n = torch.LongTensor([5, 3])
+    return rankings, ys, n
+
+
+def _monte_carlo_simulation(rankings, ys, n, click_fn, nr=100):
+    """Runs a click simulation repeatedly and reports averages.
+
+    Arguments:
+        rankings: The batch of rankings.
+        ys: The batch of relevance labels.
+        n: The nr of docs per query in the batch.
+        click_fn: The click simulate function to call.
+        nr: Number of simulations to run.
+
+    Returns:
+        The averaged output of `click_fn` across `nr` runs.
+    """
+    clicks = simulate_perfect(rankings, ys, n)
+    torch.manual_seed(4200)
+    click_aggregate = torch.zeros_like(clicks[0]).to(torch.float)
+    prop_aggregate = torch.zeros_like(clicks[1]).to(torch.float)
+    for i in range(nr):
+        clicks, props = click_fn(rankings, ys, n)
+        click_aggregate += clicks.to(dtype=torch.float)
+        prop_aggregate += props
+    return click_aggregate / float(nr), prop_aggregate / float(nr)
+
+
+def test_perfect_clicks():
+    rankings, ys, n = _generate_test_data()
+    clicks, props = _monte_carlo_simulation(rankings, ys, n, simulate_perfect)
+    rel_expected = torch.FloatTensor([
+        [0.2, 0.0, 1.0, 0.0, 0.4],
+        [1.0, 0.8, 0.0, 0.0, 0.0]
+    ])
+    props_expected = torch.FloatTensor([
+        [1.0, 1.0, 1.0, 1.0, 1.0],
+        [1.0, 1.0, 1.0, 0.0, 0.0]
+    ])
+    clicks_expected = rel_expected * props_expected
+    assert_true(torch.allclose(clicks, clicks_expected, atol=1e-01))
+    assert_true(torch.allclose(props, props_expected, atol=1e-01))
+
+
+def test_perfect_clicks_cutoff_3():
+    rankings, ys, n = _generate_test_data()
+    click_fn = lambda rankings, ys, n: simulate_perfect(
+        rankings, ys, n, cutoff=3)
+    clicks, props = _monte_carlo_simulation(rankings, ys, n, click_fn)
+    rel_expected = torch.FloatTensor([
+        [0.2, 0.0, 1.0, 0.0, 0.4],
+        [1.0, 0.8, 0.0, 0.0, 0.0]
+    ])
+    props_expected = torch.FloatTensor([
+        [1.0, 0.0, 0.0, 1.0, 1.0],
+        [1.0, 1.0, 1.0, 0.0, 0.0]
+    ])
+    clicks_expected = rel_expected * props_expected
+    assert_true(torch.allclose(clicks, clicks_expected, atol=1e-01))
+    assert_true(torch.allclose(props, props_expected, atol=1e-01))
+
+
+def test_perfect_clicks_cutoff_2():
+    rankings, ys, n = _generate_test_data()
+    click_fn = lambda rankings, ys, n: simulate_perfect(
+        rankings, ys, n, cutoff=2)
+    clicks, props = _monte_carlo_simulation(rankings, ys, n, click_fn)
+    rel_expected = torch.FloatTensor([
+        [0.2, 0.0, 1.0, 0.0, 0.4],
+        [1.0, 0.8, 0.0, 0.0, 0.0]
+    ])
+    props_expected = torch.FloatTensor([
+        [0.0, 0.0, 0.0, 1.0, 1.0],
+        [1.0, 1.0, 0.0, 0.0, 0.0]
+    ])
+    clicks_expected = rel_expected * props_expected
+    assert_true(torch.allclose(clicks, clicks_expected, atol=1e-01))
+    assert_true(torch.allclose(props, props_expected, atol=1e-01))
+
+
+def test_position_clicks():
+    rankings, ys, n = _generate_test_data()
+    clicks, props = _monte_carlo_simulation(rankings, ys, n, simulate_position)
+    rel_expected = torch.FloatTensor([
+        [0.1, 0.1, 1.0, 0.1, 0.1],
+        [1.0, 1.0, 0.1, 0.1, 0.1]
+    ])
+    props_expected = torch.FloatTensor([
+        [1/4.0, 1/6.0, 1/5.0, 1/2.0, 1/3.0],
+        [1/3.0, 1/2.0, 1/4.0, 0.0, 0.0]
+    ])
+    clicks_expected = rel_expected * props_expected
+    assert_true(torch.allclose(clicks, clicks_expected, atol=1e-01))
+    assert_true(torch.allclose(props, props_expected, atol=1e-01))
+
+
+def test_position_clicks_eta_2():
+    rankings, ys, n = _generate_test_data()
+    click_fn = lambda rankings, ys, n: simulate_position(rankings, ys, n, eta=2.0)
+    clicks, props = _monte_carlo_simulation(rankings, ys, n, click_fn)
+    rel_expected = torch.FloatTensor([
+        [0.1, 0.1, 1.0, 0.1, 0.1],
+        [1.0, 1.0, 0.1, 0.1, 0.1]
+    ])
+    props_expected = torch.FloatTensor([
+        [1/4.0, 1/6.0, 1/5.0, 1/2.0, 1/3.0],
+        [1/3.0, 1/2.0, 1/4.0, 0.0, 0.0]
+    ]) ** 2.0
+    clicks_expected = rel_expected * props_expected
+    assert_true(torch.allclose(clicks, clicks_expected, atol=1e-01))
+    assert_true(torch.allclose(props, props_expected, atol=1e-01))
+
+
+def test_position_clicks_eta_0():
+    rankings, ys, n = _generate_test_data()
+    click_fn = lambda rankings, ys, n: simulate_position(
+        rankings, ys, n, eta=0.0)
+    clicks, props = _monte_carlo_simulation(rankings, ys, n, click_fn)
+    rel_expected = torch.FloatTensor([
+        [0.1, 0.1, 1.0, 0.1, 0.1],
+        [1.0, 1.0, 0.1, 0.1, 0.1]
+    ])
+    props_expected = torch.FloatTensor([
+        [1/4.0, 1/6.0, 1/5.0, 1/2.0, 1/3.0],
+        [1/3.0, 1/2.0, 1/4.0, 0.0, 0.0]
+    ]) ** 0.0
+    props_expected[1,3] = 0.0
+    props_expected[1,4] = 0.0
+    clicks_expected = rel_expected * props_expected
+    assert_true(torch.allclose(clicks, clicks_expected, atol=1e-01))
+    assert_true(torch.allclose(props, props_expected, atol=1e-01))
+
+
+def test_position_clicks_cutoff_3():
+    rankings, ys, n = _generate_test_data()
+    click_fn = lambda rankings, ys, n: simulate_position(
+        rankings, ys, n, cutoff=3)
+    clicks, props = _monte_carlo_simulation(rankings, ys, n, click_fn)
+    rel_expected = torch.FloatTensor([
+        [0.1, 0.1, 1.0, 0.1, 0.1],
+        [1.0, 1.0, 0.1, 0.1, 0.1]
+    ])
+    props_expected = torch.FloatTensor([
+        [1/4.0, 0.0, 0.0, 1/2.0, 1/3.0],
+        [1/3.0, 1/2.0, 1/4.0, 0.0, 0.0]
+    ])
+    clicks_expected = rel_expected * props_expected
+    assert_true(torch.allclose(clicks, clicks_expected, atol=1e-01))
+    assert_true(torch.allclose(props, props_expected, atol=1e-01))
+
+
+def test_nearrandom_clicks():
+    rankings, ys, n = _generate_test_data()
+    clicks, props = _monte_carlo_simulation(rankings, ys, n, simulate_nearrandom)
+    rel_expected = torch.FloatTensor([
+        [0.45, 0.4, 0.6, 0.4, 0.5],
+        [0.6, 0.55, 0.4, 0.4, 0.4]
+    ])
+    props_expected = torch.FloatTensor([
+        [1/4.0, 1/6.0, 1/5.0, 1/2.0, 1/3.0],
+        [1/3.0, 1/2.0, 1/4.0, 0.0, 0.0]
+    ])
+    clicks_expected = rel_expected * props_expected
+    assert_true(torch.allclose(clicks, clicks_expected, atol=1e-01))
+    assert_true(torch.allclose(props, props_expected, atol=1e-01))
+
+
+def test_nearrandom_clicks_eta_2():
+    rankings, ys, n = _generate_test_data()
+    click_fn = lambda rankings, ys, n: simulate_nearrandom(
+        rankings, ys, n, eta=2.0)
+    clicks, props = _monte_carlo_simulation(rankings, ys, n, click_fn)
+    rel_expected = torch.FloatTensor([
+        [0.45, 0.4, 0.6, 0.4, 0.5],
+        [0.6, 0.55, 0.4, 0.4, 0.4]
+    ])
+    props_expected = torch.FloatTensor([
+        [1/4.0, 1/6.0, 1/5.0, 1/2.0, 1/3.0],
+        [1/3.0, 1/2.0, 1/4.0, 0.0, 0.0]
+    ]) ** 2.0
+    clicks_expected = rel_expected * props_expected
+    assert_true(torch.allclose(clicks, clicks_expected, atol=1e-01))
+    assert_true(torch.allclose(props, props_expected, atol=1e-01))
+
+
+def test_nearrandom_clicks_eta_0():
+    rankings, ys, n = _generate_test_data()
+    click_fn = lambda rankings, ys, n: simulate_nearrandom(
+        rankings, ys, n, eta=0.0)
+    clicks, props = _monte_carlo_simulation(rankings, ys, n, click_fn)
+    rel_expected = torch.FloatTensor([
+        [0.45, 0.4, 0.6, 0.4, 0.5],
+        [0.6, 0.55, 0.4, 0.4, 0.4]
+    ])
+    props_expected = torch.FloatTensor([
+        [1/4.0, 1/6.0, 1/5.0, 1/2.0, 1/3.0],
+        [1/3.0, 1/2.0, 1/4.0, 0.0, 0.0]
+    ]) ** 0.0
+    props_expected[1,3] = 0.0
+    props_expected[1,4] = 0.0
+    clicks_expected = rel_expected * props_expected
+    assert_true(torch.allclose(clicks, clicks_expected, atol=1e-01))
+    assert_true(torch.allclose(props, props_expected, atol=1e-01))
+
+
+def test_nearrandom_clicks_cutoff_3():
+    rankings, ys, n = _generate_test_data()
+    click_fn = lambda rankings, ys, n: simulate_nearrandom(
+        rankings, ys, n, cutoff=3)
+    clicks, props = _monte_carlo_simulation(rankings, ys, n, click_fn)
+    rel_expected = torch.FloatTensor([
+        [0.45, 0.4, 0.6, 0.4, 0.5],
+        [0.6, 0.55, 0.4, 0.4, 0.4]
+    ])
+    props_expected = torch.FloatTensor([
+        [1/4.0, 0.0, 0.0, 1/2.0, 1/3.0],
+        [1/3.0, 1/2.0, 1/4.0, 0.0, 0.0]
+    ])
+    clicks_expected = rel_expected * props_expected
+    assert_true(torch.allclose(clicks, clicks_expected, atol=1e-01))
+    assert_true(torch.allclose(props, props_expected, atol=1e-01))
