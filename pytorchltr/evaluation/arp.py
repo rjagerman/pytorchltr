0c8d235a8a853dc64ece980d171455420986d3d7
Rolf Jagerman <rjagerman@gmail.com>
2019-12-07 19:13:00 +0100

    Fix dtype in ARP

--- pytorchltr/evaluation/arp.py
+++ pytorchltr/evaluation/arp.py
@@ -21,7 +21,9 @@ def arp(scores, relevance, n):
     # Compute relevance per rank
     rel_sort = _torch.gather(relevance, 1, _rank_by_score(scores, n)).float()
     arange = 1.0 + _torch.repeat_interleave(
-        _torch.arange(rel_sort.shape[1], device=rel_sort.device)[None, :],
+        _torch.arange(
+            rel_sort.shape[1], device=rel_sort.device,
+            dtype=_torch.float)[None, :],
         rel_sort.shape[0], dim=0)
     srp = _torch.sum(arange * rel_sort, dim=1)
     nrp = _torch.sum(rel_sort, dim=1)
