5a70df437c6e4b04e2b4c79b657b5d31464d7bf0
Rolf Jagerman <rjagerman@gmail.com>
2019-12-12 12:16:39 +0100

    Optimize pairwise loss slightly and add basic tests

--- pytorchltr/loss/pairwise.py
+++ pytorchltr/loss/pairwise.py
@@ -41,26 +41,21 @@ class AdditivePairwiseLoss(_torch.nn.Module):
         loss[loss < 0.0] = 0.0
 
         # Mask out padded documents per query in the batch
-        n_grid = n.reshape((n.shape[0], 1, 1))
-        n_grid = _torch.repeat_interleave(n_grid, s_ij.shape[1], dim=1)
-        n_grid = _torch.repeat_interleave(n_grid, s_ij.shape[2], dim=2)
-        range_grid = _torch.max(*_torch.meshgrid(
-            [_torch.arange(s_ij.shape[1], device=s_ij.device),
-             _torch.arange(s_ij.shape[2], device=s_ij.device)]))
-        range_grid = range_grid.reshape(
-            (1, range_grid.shape[0], range_grid.shape[1]))
-        range_grid = _torch.repeat_interleave(range_grid, n.shape[0], dim=0)
+        n_grid = n[:, None, None].repeat(1, s_ij.shape[1], s_ij.shape[2])
+        arange = _torch.arange(s_ij.shape[1], device=s_ij.device)
+        range_grid_1 = _torch.max(*_torch.meshgrid([arange, arange]))
+        range_grid = range_grid_1[None, :, :].repeat(n.shape[0], 1, 1)
         loss[n_grid <= range_grid] = 0.0
 
         # Reduce final list loss by sum, creating upper bound on rel result ranks
         loss = loss.view(loss.shape[0], -1)
-        loss = 1.0 + loss.sum(1)
+        loss = loss.sum(1)
 
         # Apply a loss modifier
         if self.loss_modifier == "normrank":
             loss /= n.to(dtype=loss.dtype)
         elif self.loss_modifier == "dcg":
-            loss = -1.0 / _torch.log(1.0 + loss)
+            loss = -1.0 / _torch.log(2.0 + loss)
 
         # Return loss
         return loss
---
+++ tests/loss/test_pairwise.py
@@ -0,0 +1,70 @@
+import torch
+from pytorchltr.loss.pairwise import AdditivePairwiseLoss
+from pytest import approx
+
+
+def test_pairwise_rank_perfect():
+    loss_fn = AdditivePairwiseLoss("rank")
+    scores = torch.FloatTensor([[[0.0], [0.0], [1.0], [2.0], [1.0]]])
+    ys = torch.LongTensor([[0, 0, 1, 2, 1]])
+    n = torch.LongTensor([5])
+    loss = loss_fn(scores, ys, n)
+
+    # RankLoss: 1.0 + \sum_{(d_i, d_j) : y_i > y_j} hinge(f(d_j) - f(d_i)))
+    assert loss.item() == approx(0.0)
+
+
+def test_pairwise_rank_2():
+    loss_fn = AdditivePairwiseLoss("rank")
+    scores = torch.FloatTensor([[[0.0], [0.0], [1.0], [1.0], [1.0]]])
+    ys = torch.LongTensor([[0, 0, 1, 2, 1]])
+    n = torch.LongTensor([5])
+    loss = loss_fn(scores, ys, n)
+
+    # RankLoss: 1.0 + \sum_{(d_i, d_j) : y_i > y_j} hinge(f(d_j) - f(d_i)))
+    assert loss.item() == approx(2.0)
+
+
+def test_pairwise_rank_3():
+    loss_fn = AdditivePairwiseLoss("rank")
+    scores = torch.FloatTensor([[[0.0], [0.0], [1.0], [-5.0], [1.0]]])
+    ys = torch.LongTensor([[0, 0, 1, 2, 1]])
+    n = torch.LongTensor([5])
+    loss = loss_fn(scores, ys, n)
+
+    # RankLoss: 1.0 + \sum_{(d_i, d_j) : y_i > y_j} hinge(f(d_j) - f(d_i)))
+    assert loss.item() == approx(7.0 + 7.0 + 6.0 + 6.0)
+
+
+def test_pairwise_rank_batch():
+    loss_fn = AdditivePairwiseLoss("rank")
+    scores = torch.FloatTensor([
+        [[0.0], [10.0], [1.0], [0.5], [1.0]],
+        [[1.0], [3.5], [6.0], [4.3], [10.0]]])
+    ys = torch.LongTensor([
+        [0, 2, 1, 2, 1],
+        [1, 2, 2, 1, 0]])
+    n = torch.LongTensor([5, 4])
+    loss = loss_fn(scores, ys, n)
+
+    # RankLoss: 1.0 + \sum_{(d_i, d_j) : y_i > y_j} hinge(f(d_j) - f(d_i)))
+    assert loss[0].item() == approx(0.5 + 1.5 + 1.5)
+    assert loss[1].item() == approx(5.3 - 3.5)
+
+
+def test_pairwise_rank_cutoff():
+    loss_fn = AdditivePairwiseLoss("rank")
+    scores = torch.FloatTensor([[[1.0], [3.5], [6.0], [4.3], [8.0]]])
+    ys = torch.LongTensor([[1, 2, 2, 1, 0]])
+    n1 = torch.LongTensor([3])
+    n2 = torch.LongTensor([4])
+    n3 = torch.LongTensor([5])
+    loss1 = loss_fn(scores, ys, n1)
+    loss2 = loss_fn(scores, ys, n2)
+    loss3 = loss_fn(scores, ys, n3)
+
+    # RankLoss: 1.0 + \sum_{(d_i, d_j) : y_i > y_j} hinge(f(d_j) - f(d_i)))
+    assert loss1.item() == approx(0.0)
+    assert loss2.item() == approx(5.3 - 3.5)
+    assert loss3.item() == approx(
+        (5.3 - 3.5) + (9.0 - 4.3) + (9.0 - 6.0) + (9.0 - 3.5) + (9.0 - 1.0))
