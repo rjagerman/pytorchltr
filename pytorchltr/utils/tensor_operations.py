bd1258e5a5d6128940369f41dd9b3c0c826b72db
Rolf Jagerman <rjagerman@gmail.com>
2020-06-10 19:24:19 +0200

    Add plackett luce sampling (#13)

--- pytorchltr/utils/tensor_operations.py
+++ pytorchltr/utils/tensor_operations.py
@@ -5,7 +5,7 @@ import torch as _torch
 def mask_padded_values(xs, n, mask_value=-float('inf'), mutate=False):
     """Turns padded values into given mask value.
 
-    Arguments:
+    Args:
         xs: A tensor of size (batch_size, list_size, 1) containing padded
             values.
         n: A tensor of size (batch_size) containing list size of each query.
@@ -23,18 +23,19 @@ def mask_padded_values(xs, n, mask_value=-float('inf'), mutate=False):
     return xs
 
 
-def tiebreak_argsort(x):
+def tiebreak_argsort(x, descending=True):
     """Computes a per-row argsort of matrix x with random tiebreaks.
 
-    Arguments:
+    Args:
         x: A 2D tensor where each row will be argsorted.
+        descending: Whether to sort in descending order.
 
     Returns:
         A 2D tensor of the same size as x, where each row is the argsort of x,
         with ties broken randomly.
     """
     p = _torch.randperm(x.shape[1], device=x.device)
-    return p[_torch.argsort(x[:, p], descending=True)]
+    return p[_torch.argsort(x[:, p], descending=descending)]
 
 
 def rank_by_score(scores, n):
@@ -51,6 +52,32 @@ def rank_by_score(scores, n):
     if scores.dim() == 3:
         scores = scores.reshape((scores.shape[0], scores.shape[1]))
     return tiebreak_argsort(mask_padded_values(scores, n))
+
+
+def rank_by_plackettluce(scores, n):
+    """Samples a ranking from a plackett luce distribution.
+
+    This method ensures that padded documents are placed last.
+
+    Args:
+        scores: A tensor of size (batch_size, list_size, 1) or
+                (batch_size, list_size) containing scores.
+        n: A tensor of size (batch_size) containing list size of each query.
+    """
+    if scores.dim() == 3:
+        scores = scores.reshape((scores.shape[0], scores.shape[1]))
+    masked_scores = mask_padded_values(scores, n)
+
+    # This implementation uses reservoir sampling, which comes down to doing
+    # Uniform(0, 1) ^ (1 / p) and then sorting by the resulting values. The
+    # following implementation is a numerically stable variant that operates in
+    # log-space.
+    log_p = _torch.nn.LogSoftmax(dim=1)(masked_scores)
+    c_zero = _torch.tensor(0.0).to(device=scores.device)
+    c_one = _torch.tensor(1.0).to(device=scores.device)
+    u = _torch.distributions.uniform.Uniform(c_zero, c_one).sample(log_p.shape)
+    r = _torch.log(-_torch.log(u)) - log_p
+    return tiebreak_argsort(r, descending=False)
 
 
 def batch_pairwise_difference(x):
