798c137b864ed7e83038111dfb6c3bb63afe0f36
Rolf Jagerman <rjagerman@gmail.com>
2020-07-03 12:41:42 +0200

    Update setup.py with additional meta data

--- setup.py
+++ setup.py
@@ -2,18 +2,30 @@ from setuptools import setup
 from setuptools import find_packages
 
 
+with open("README.md", "rt") as f:
+    long_description = f.read()
+
+
 setup(
     name="pytorchltr",
     version="0.1.0",
     description="Learning to Rank with PyTorch",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/rjagerman/pytorchltr",
     author="Rolf Jagerman",
     author_email="rjagerman@gmail.com",
     license="MIT",
     packages=find_packages(exclude=("tests", "tests.*",)),
+    python_requires='>=3.6',
     install_requires=["numpy",
                       "scikit-learn",
                       "scipy",
                       "torch"],
-    tests_require=["pytest"]
+    tests_require=["pytest"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
 )
