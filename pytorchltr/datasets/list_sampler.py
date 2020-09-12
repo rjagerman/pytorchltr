c0665c055bbd4dc5d7b521bb7bfbaf41412d848e
Rolf Jagerman <rjagerman@gmail.com>
2020-09-12 18:08:43 +0200

    Tiny optimization for balanced rel sampler and more comments

--- pytorchltr/datasets/list_sampler.py
+++ pytorchltr/datasets/list_sampler.py
@@ -34,17 +34,28 @@ class BalancedRelevanceSampler(UniformSampler):
         super().__init__(max_list_size, rng)
 
     def __call__(self, relevance: _torch.LongTensor) -> _torch.LongTensor:
+        # Get the unique relevance grades and randomly permute them
         unique_rel = _torch.unique(relevance)
+        unique_rel = unique_rel[_torch.randperm(
+            unique_rel.shape[0], **self.rng_kw)]
+
+        # Randomly shuffle the relevance grades
+        perm = _torch.randperm(relevance.shape[0], **self.rng_kw)
+        rel_shuffled = relevance[perm]
+
+        # Get maximum size and create a stack for output
         max_size = self.max_list_size(relevance)
         stacked_rel = -_torch.ones((unique_rel.shape[0], relevance.shape[0]),
                                    dtype=_torch.long, device=relevance.device)
-        perm = _torch.randperm(relevance.shape[0], **self.rng_kw)
-        rel_shuffled = relevance[perm]
-        unique_perm = _torch.randperm(unique_rel.shape[0], **self.rng_kw)
-        for j, rel in enumerate(unique_rel[unique_perm]):
+
+        # For each unique relevance grade fill a row in the stacked_rel matrix
+        # with the indices of that relevance grade
+        for j, rel in enumerate(unique_rel):
             idxs = _torch.where(rel_shuffled == rel)[0]
             size = min(max_size, idxs.shape[0])
             stacked_rel[j, 0:size] = idxs[0:size]
+
+        # Create output by removing unused indices and undoing the permutation
         out = stacked_rel.T.reshape(relevance.shape[0] * unique_rel.shape[0])
         out = out[out != -1]
         out = perm[out]
