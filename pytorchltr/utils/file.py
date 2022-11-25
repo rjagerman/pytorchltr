4cec01ec9b46215ae63e800dc9d18fddc819e65b
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-25 18:17:15 +0000

    Adding tarfile member sanitization to extractall()

--- pytorchltr/utils/file.py
+++ pytorchltr/utils/file.py
@@ -156,7 +156,26 @@ def extract_tar(path: str, destination: str):
     """
     logging.info("extracting tar file at '%s' to '%s'", path, destination)
     with tarfile.open(path) as f:
-        f.extractall(destination)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(f, destination)
 
 
 def extract_zip(path: str, destination: str):
